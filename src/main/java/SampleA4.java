3756799874438cf8536e3fbc71a95c422473c23a
User <User@Arshiya>
2024-09-03 14:55:47 +0530

    commit done by test engineer 3

---
+++ src/main/java/SampleA4.java
@@ -0,0 +1,9 @@
+
+
+public class SampleA4 {
+
+	public void method()
+	{
+		System.out.println("Welcome to git");
+	}
+}
---
+++ src/main/java/SampleProp/SampleA4.java
@@ -0,0 +1,9 @@
+package SampleProp;
+
+public class SampleA4 {
+
+	public void method()
+	{
+		System.out.println("Welcome to git");
+	}
+}
